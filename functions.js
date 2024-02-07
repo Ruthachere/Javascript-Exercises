da5c17a02af0bd97457da47be78273cbbe5bc2a2
Ruthachere <akoruth22@gmail.com>
2024-02-07 10:13:57 +0100

    Function to add all numbers in a sequence

--- functions.js
+++ functions.js
@@ -43,3 +43,12 @@ function countBs(string) {
 console.log(countBs("BbkjkcjxkjBC"));
 
 console.log(countChar("Honorine", "n"));
+
+function sum(n) {
+ if (n <= 1) {
+    return n;
+ } else {
+   return n + sum(n-1);
+ }
+}
+console.log(sum(3));
